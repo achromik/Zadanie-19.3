99c084cb686124451d7ea6a354ea99aea226bfbf
Aleksander Chromik <achromik@gmail.com>
2017-11-08 14:04:12 +0100

    CREATE action's objects

---
+++ actions.js
@@ -0,0 +1,45 @@
+import uuid from 'uuid'
+
+const   EDIT_COMMENT = 'EDIT_COMMENT',
+        CREATE_COMMENT = 'CREATE_COMMENT',
+        DELETE_COMMENT = 'DELETE_COMMENT',
+        THUMB_UP_COMMENT = 'THUMB_UP_COMMENT',
+        THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function editComment(id, text) {
+    return {
+        type: EDIT_COMMENT,
+        id,
+        text
+    }
+}
+
+function createComment(text) {
+    return {
+        type: CREATE_COMMENT,
+        id: uuid.v4
+    }
+}
+
+function deleteComment(id) {
+    return {
+        type: DELETE_COMMENT,
+        id
+    }
+}
+
+function thumbUpComment(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        thumbsUp: thumbsUp + 1
+    }
+}
+
+function thumbDownComment(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        thumbsDown: thumbsDown + 1
+    }
+}
